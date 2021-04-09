2beb30350f82acaf72aa3f8e042d8376b01bd276
KcWebers <kweberski@gmail.com>
2021-04-08 20:01:52 -0700

    Student work balanceIndex()

--- Chapter06_ArraysPt2/balanceIndex.js
+++ Chapter06_ArraysPt2/balanceIndex.js
@@ -38,3 +38,76 @@ console.log(balanceIndex([9, 9])); // -1
 // ============================
 // Students' Solutions
 // ============================
+
+function balanceIndex(arr) {
+    // your code here
+    var next = 1;
+    var sum1 = 0;
+    var sum2 = 0;
+    var found = -1;
+    for (var i = 0; i< arr.length; i++){
+        for(var j = next-1; j>=0; j--){
+            sum1 += arr[j]
+        }
+        for(var k = next+1; k < arr.length; k++){
+            sum2 += arr[k];
+        }
+
+        if(sum1 == sum2){
+            found = next
+            break
+        }
+        else{
+            found = -1
+        }
+        next +=1
+        sum1 = 0;
+        sum2 = 0;
+    }
+    return found
+}
+
+// ============================
+// returns true/false instead of the index where the balance is
+
+function balanceIndex(arr) {
+    // your code here
+    sum1 = 0;
+    sum2 = 0;
+    if(arr.length % 2 == 0){
+        return -1;
+    }
+    for(i=0; i < Math.floor(arr.length/2); i++){
+        console.log(arr[i]);
+        sum1 += arr[i];
+        console.log("sum1 is: " + sum1);
+    }
+    for(i=arr.length-1; i > Math.floor(arr.length/2); i--){
+        console.log(arr[i]);
+        sum2 += arr[i];
+        console.log("sum2 is: " + sum2);
+    }
+    if(sum1 == sum2){
+        console.log("${sum1} equals ${sum2}");
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+// ============================
+
+function balanceIndex(arr) {
+    if (arr.length < 3) return -1;
+    const reducer = (acc, curVal) => acc + curVal;
+    for (let i = 0; i < arr.length - 2; i++) {
+      const left = arr.slice(0, i + 1).reduce(reducer);
+      const right = arr.slice(i + 2).reduce(reducer);
+      if (left === right) return i + 1;
+    }
+    return -1;
+  }
+
+
+
