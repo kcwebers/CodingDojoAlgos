c8d6ea3be8cefc4088edcf927caf55b7aeb8050f
KcWebers <kweberski@gmail.com>
2021-04-08 20:42:35 -0700

    Addition to flattenArray()

--- Chapter06_ArraysPt2/flattenArray.js
+++ Chapter06_ArraysPt2/flattenArray.js
@@ -111,3 +111,26 @@ function flatten(arr) {
     return newArr;
 }
 
+// ============================
+
+const flatten = (arr) => {
+    for(let i= 0; i < arr.length; i++){
+        if(typeof arr[i] === 'object'){
+            if(arr[i].length === 0){
+                console.log('[] found')
+                arr.splice(i, 1);
+            } else {
+                let temp = [];
+                for(let j=0; j< arr[i].length; j++){
+                    temp.push(arr[i][j]);
+                }
+                arr.splice(i, 1)
+                let arr2 = arr.splice(i)
+                arr = arr.concat(temp,arr2)
+            }
+            
+        }
+    }
+    return(arr)
+};
+
