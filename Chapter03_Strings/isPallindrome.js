5fb07047b1591ef488e65d7599e2410a24b8b6cd
KcWebers <kweberski@gmail.com>
2021-05-05 07:12:02 -0700

    isPallindrome

---
+++ Chapter03_Strings/isPallindrome.js
@@ -0,0 +1,19 @@
+// Create a function that returns as boolean of true/false for whether or not an input string is a strict pallindrome. Do not ignore whitespaces!!
+
+// Example 1: "racecar" --> true
+// Example 2: "Dud" --> false
+// Example 3: "oho!" --> false
+
+function isPallindrome(str) {
+    for (var i = 0 ; i < str.length/2-1 ; i ++) {
+        if(str[i] !== str[str.length-1-i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+console.log(isPallindrome("racecar"));
+console.log(isPallindrome("Dud"));
+console.log(isPallindrome("oho!"));
---
+++ Chapter03_Strings/longestPallindrome.js
@@ -0,0 +1,13 @@
+// Given a String, return the longest pallindromic substring. Given "hello dada", return "dad". Given "not much" return "n". Include spaces as well!
+
+// Example 1: "my favorite racecar erupted" --> "e racecar e"
+// Example 2: "nada" --> "ada"
+// Example 3: "nothing to see" --> "ee"
+
+function longestPallindrome(str) {
+    // your code here
+}
+
+console.log(longestPallindrome("my favorite racecar erupted"));
+console.log(longestPallindrome("nada"));
+console.log(longestPallindrome("nothing to see"));
