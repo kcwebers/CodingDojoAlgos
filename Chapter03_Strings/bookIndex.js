286f0d765c6be7d01c12a25fc30e9e501d384d7e
KcWebers <kweberski@gmail.com>
2021-05-06 08:27:34 -0700

    bookIndex

---
+++ Chapter03_Strings/bookIndex.js
@@ -0,0 +1,27 @@
+// Write a function that given a sorted array of page numbers, return a string representing a book index. Combine consecutive pages to create ranges.
+
+// Example: [1,3,4,5,6,7,8,10] --> "1, 3-8, 10"
+
+function bookIndex(arr){
+    var str = "";
+    for (var i = 0 ; i < arr.length ; i++){
+        if (i < arr.length && i !== 0){
+            str += ", ";
+        }
+        if (arr[i + 1] === arr[i] + 1){
+            var start = arr[i];
+            while (arr[i + 1] === arr[i]+1){
+                i++;
+            }
+            var end = arr[i];
+            str += start + "-" + end;
+        }
+        else {
+            str += arr[i];
+        }
+    }
+    return str;
+}
+var str = "hello"
+str.slice(1, 3)
+console.log(bookIndex([1,3,4,5,6,7,8,10]))
