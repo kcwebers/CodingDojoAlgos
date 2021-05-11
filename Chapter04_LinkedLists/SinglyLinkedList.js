5232221e7fcd9cd664ea7462eb3c048be1d913f0
KcWebers <kweberski@gmail.com>
2021-05-11 07:39:36 -0700

    Initial functions update

--- Chapter04_LinkedLists/SinglyLinkedList.js
+++ Chapter04_LinkedLists/SinglyLinkedList.js
@@ -20,17 +20,52 @@ class SLList{
     // given a value, add it to the back of your singly linked list
     // what if the list is empty?
     addToBack(value) {
-        // your code here
+        var node = new Node(value);
+        if(!this.head) {
+            this.head = node;
+        }
+        var runner = this.head;
+        while(runner.next) {
+            runner = runner.next;
+        }
+        runner.next = node;
     }
 
     // given a value, print whether the list contains that value
     contains(value) {
+        if(!this.head) {
+            return false;
+        }
+        var runner = this.head;
+        while(runner) {
+            if(runner.value === value) {
+                return true;
+            }
+            runner = runner.next;
+        }
+        return false;
+    }
+    
+    // remove the first node in the list
+    removeFromFront() {
+        // your code here
+    }
+
+    // remove the last node in the list
+    removeFromBack() {
         // your code here
     }
     
     // print the singly linked list
     printValues() {
-        // your code here
+        if(!this.head) {
+            console.log("There's nothing in this list!");
+        }
+        var runner = this.head;
+        while(runner) {
+            console.log(`${runner.value} --> `);
+            runner = runner.next;
+        }
     }
 
 }
