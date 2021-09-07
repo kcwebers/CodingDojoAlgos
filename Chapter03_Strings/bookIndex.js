835a0b4d68955f03196165a3b087b94f3c01e7e8
kcwebers <kweberski@gmail.com>
2021-09-06 18:39:26 -0700

    Student work 9.2.2021

--- Chapter03_Strings/bookIndex.js
+++ Chapter03_Strings/bookIndex.js
@@ -64,6 +64,7 @@ function bookIndex(arr) {
 
 console.log(bookIndex([1,3,4,5,6,7,8,10]))
 
+// ============================
 
 function bookIndex(arr){
     var preOutput = "";
@@ -82,6 +83,7 @@ function bookIndex(arr){
 }
 console.log(bookIndex([1,3,4,5,7,8,10]))
 
+// ============================
 
 function bookIndex(input) {
     var stringarray = [];
@@ -106,6 +108,8 @@ function bookIndex(input) {
     return buildIndex(stringarray);
 }
 
+// ============================
+
 function buildIndex(input) {
     var output = "";
     for (var i = 0; i < input.length; i++) {
@@ -120,3 +124,102 @@ function buildIndex(input) {
 console.log(bookIndex([1, 3, 4, 5, 6, 7, 8, 10]));
 console.log(bookIndex([1, 2, 3, 4, 5]));
 console.log(bookIndex([1, 3, 5, 11, 12, 13, 21, 22, 23, 24, 89, 255, 256, 257, 314]));
+
+// ============================
+
+function bookIndex(arr){
+    //your code here
+    //iterate through the string
+    for(var i = 0; i < arr.length; i++){
+        //search for consecutive numbers
+        if(arr[i]+1 == arr[i+1]){
+            var j = 1 // marker to increase during the while loop
+            while(arr[i]+j == arr[i+1]){
+                //if consecutive numbers find starting and ending indexes. 
+                var temp = arr[i+1]
+                //erase indexes no longer needed
+                arr.splice(i+1, 1)
+                //Combine values with a '-' in between
+                j++
+            }
+            arr[i] = `${arr[i]}-${temp}`
+        }
+    }
+    return arr.join(', ')
+}
+
+console.log(bookIndex([1,3,4,5,7,8,10,12]))
+
+// ============================
+
+function bookIndex(arr) {
+    var currentPg=0;
+    var hyphen=false;
+    var output="";
+
+    for(var i=0; i<arr.length; i++) {
+        if(currentPg == 0 || currentPg != arr[i]-1) {
+            output += arr[i];
+        }
+        else if(currentPg == arr[i]-1 && arr[i+1] != arr[i]+1){
+            output += "-" + arr[i];
+        }
+
+        if((arr[i+1] != arr[i]+1) && (i != arr.length-1)) {
+            output += ", ";
+        }
+
+        currentPg=arr[i]
+    }
+    return output;
+}
+
+console.log(bookIndex([1,3,4,5,7,8,10,12]));
+
+// ============================
+
+function bookIndex(arr) {
+    var index = ''
+    arr.forEach((val, idx) => {
+        if(idx === 0 || val !== arr[idx-1]+1)
+            index = index + ", " + val 
+        else if (idx === arr.length-1 || val === arr[idx-1]+1){
+            index.charAt(index.length-(String(arr[idx-1]).length+1)) == '-' ?
+            index = index.substring(0,index.lastIndexOf('-')+1)+val :
+            index = index + '-' + val
+        }
+    })
+    return index.substring(2)
+}
+
+console.log(bookIndex([1,3,4,5,7,8,10,12]))
+console.log(bookIndex([1,2,3,4,5,6,7,8,9,10,11,12]))
+console.log(bookIndex([1,3,4,5,7,8,10,12,100,101,102,104,105,106,107]))
+
+// ============================
+
+function bookIndex(arr){
+    var comb = []
+    var start
+    var end
+    for(var i=0;i<=arr.length -1; i++){
+      start= arr[i]
+      end = start
+  
+      while (arr[i+1]- arr[i]==1) {
+        end = arr[i+1]
+        i++
+      }
+      // 
+      if(start == end){
+        comb.push(start)
+      }else{
+        comb.push(start+ "-"+ end)
+      }
+  
+    }
+    return comb.join(', ')
+  }
+
+  console.log(bookIndex([1,3,4,5,7,8,10,12]))
+
