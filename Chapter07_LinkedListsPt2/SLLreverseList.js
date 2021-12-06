d493ad5a41b71260961e8ad1103874c2f9722c25
kcwebers <kweberski@gmail.com>
2021-12-06 08:34:34 -0800

    SLL Pt 2 reverseList

--- Chapter07_LinkedListsPt2/SLLreverseList.js
+++ Chapter07_LinkedListsPt2/SLLreverseList.js
@@ -1,39 +1,39 @@
-class Node {
-    constructor(value) {
-        this.val = value;
+class SLNode{
+    constructor(value){
+        this.value = value;
         this.next = null;
     }
 }
 
-class SLL {
-    constructor() {
+class SLL{
+    constructor(){
         this.head = null;
     }
 
-    addToFront(value) {
-        var newNode = new Node(value);
+    addToFront(value){
+        var newNode = new SLNode(value);
         newNode.next = this.head;
         this.head = newNode;
     }
 
     addToBack(value) {
-        var newNode = new Node(value);
-        if (!this.head) {
+        var newNode = new SLNode(value);
+        if(!this.head) {
             this.head = newNode;
         } else {
             var runner = this.head;
-            while (runner.next) {
+            while (runner.next){
                 runner = runner.next;
             }
             runner.next = newNode;
         }
     }
 
-    // Given a Singly Linked List of Nodes, reverse the order of the list
-    //           this.head
-    // Example:      4 --> -12 --> 43 --> 8 --> 67 --> -27 --> 19 --> null
-    //           this.head
-    // Becomes:     19 --> -27 --> 67 --> 8 --> 43 --> -12 --> 4 --> null
+// Given a Singly Linked List of Nodes, reverse the order of the list
+//           this.head
+// Example:      4 --> -12 --> 43 --> 8 --> 67 --> -27 --> 19 --> null
+//           this.head
+// Becomes:     19 --> -27 --> 67 --> 8 --> 43 --> -12 --> 4 --> null
 
     reverseList() {
         if (!this.head || this.head.next == null) {
@@ -43,15 +43,23 @@ class SLL {
         var currentNode = this.head;
         var nextNode = this.head.next;
         var prevNode = null;
+        // console.log("currentNode = " + currentNode.value)
+        // console.log("nextNode = " + nextNode.value)
+        // console.log("prevNode = " + prevNode)
 
-        while (currentNode) {
+        while(currentNode) {
             nextNode = currentNode.next;
             currentNode.next = prevNode;
             prevNode = currentNode;
             currentNode = nextNode;
+            // console.log("currentNode = " + currentNode?.value)
+            // console.log("nextNode = " + nextNode?.value)
+            // console.log("prevNode = " + prevNode.value)
+            // console.log("--------------------------")
         }
         this.head = prevNode;
     }
+
 
     // =================================
     //  student solutions
