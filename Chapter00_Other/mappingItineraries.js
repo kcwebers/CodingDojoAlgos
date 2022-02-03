db049efc2add4dd360be1ffd6d95f2d578929342
kcwebers <kweberski@gmail.com>
2022-02-03 11:06:25 -0800

    mapping itineraries

--- Chapter00_Other/mappingItineraries.js
+++ Chapter00_Other/mappingItineraries.js
@@ -130,3 +130,32 @@ function mappingItineraries(arr) {
     finalString += arr[j][1]; //Now we add the last stop
     return finalString;
 }
+
+// ============================================
+
+function mappingItineraries(arr) {
+    var result = []
+    var runner1 = 0;
+    var runner2 = 0;
+    while(runner2 < arr.length) {
+        if(arr[runner1][0] == arr[runner2][1]) {
+            runner1++;
+            runner2 = 0;
+        }else if(runner2 == arr.length-1) {
+            result.push(arr[runner1][0]);
+            runner1 = 0;
+            runner2 ++;
+        }else {
+            runner2 ++;
+        }
+    }
+    while(result.length <= arr.length) {
+        if(result[result.length-1] == arr[runner1][0]) {
+            result.push(arr[runner1][1]);
+            runner1 = 0;
+        }else {
+            runner1++
+        }
+    }
+    return result.join(" -> ");
+}
