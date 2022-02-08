eb1e6d75c5903a465ca81226108c924af507205a
kcwebers <kweberski@gmail.com>
2022-02-08 08:49:30 -0800

    bracesValid

--- Chapter03_Strings/bracesValid.js
+++ Chapter03_Strings/bracesValid.js
@@ -378,3 +378,97 @@ function bracesValid(string) {
 }
 
 // ================================================================
+
+// Complex
+function bracesValid(str){
+    //empy arr
+    var inStr = [];
+    // elements checking for in parameter string
+    var elements = ["(",")","{","}","[","]"];
+    //looking at each incv element in parameter
+    for(i=0;i<str.length; i++){
+        //checks that char compare to each of the elements in elements arr
+        for(k=0;k<elements.length;k++){
+            //compare - if they're == pushes item into array inStr
+            if(str[i] == elements[k]){
+                inStr.push(elements[k]);
+            }
+        }
+    }
+
+    // on/off switch
+    var valid;
+
+    // if its odd automatically false
+    if(inStr.length % 2 == 0){
+        // loops through checking pairs first to last || element to element next to
+        for(b = 0; b < inStr.length / 2; b++){
+            //lisa: put me into a for loop
+            if(inStr[b] == "(" && (inStr[inStr.length - 1 - b] == ")") ||
+            inStr[b] == ")"){
+                valid = true;
+            }else if(inStr[b] == "{" && (inStr[inStr.length - 1 - b] == "}") || inStr[b] == "}" ){
+                valid = true;
+            }else if(inStr[b] == "[" && inStr[inStr.length - 1 - b] == "]" ||
+            inStr[b] == "]"){
+                valid = true;
+            }else{
+                valid = false;
+            }
+        }
+    }else{
+        valid = false;
+    }
+    return valid;
+}
+
+
+// calling function
+// console.log(bracesValid("({[({})]})"));
+// console.log(bracesValid("d(i{a}l[t]o)n{e!"));
+console.log(bracesValid("{{[a]}}(){bcd}{()}"));
+
+// Example 1: "({[({})]})" --> true
+// Example 2: "d(i{a}l[t]o)n{e!" --> false
+// Example 2: "{{[a]}}(){bcd}{()}" --> true
+
+// ================================================================
+
+let stack = []
+var string = "[()({aaa7})]"
+var t = false;
+
+
+for (let i = 0; i < string.length; i++) {
+    if (string[i] == ')' || string[i] == '}' || string[i] == ']') {
+        if (stack.length == 0) {
+            break;
+        }
+        var s = stack.pop()
+
+        stack.push(s)
+        if (string[i] == ')' && s == ('(')) {
+            t = true;
+            stack.pop()
+
+        } else if (string[i] == '}' && s == '{') {
+            t = true;
+            stack.pop()
+        } else if (string[i] == ']' && s == '[') {
+            t = true;
+            stack.pop()
+        } else {
+            t = false;
+            break;
+        }
+    } else {
+
+        if (string[i] == '(' || string[i] == '{' || string[i] == '[')
+            stack.push(string[i])
+    }
+}
+if (stack.length == 0)
+    console.log(t)
+else console.log(t)
+
+// ================================================================
--- board.js
+++ board.js
@@ -1,74 +1,3 @@
-// Given a list of itineraries, find the proper path of the trip. 
-// A list of itineraries will be an array of arrays, where the inner array will always be length two. 
-// For example, here is an example list of itineraries: [ ['LAX', 'SFO'], ['ICN', 'LAX'], ['SJC', 'ICN'] ].
-
-// The first element in each inner array is the "from" airport, and the latter is the "to" airport. 
-// So, ['LAX', 'SFO'] means "from LAX to SFO." Running with this example, given this list of itineraries,
-// your code should then print out 'SJC -> ICN -> LAX -> SFO', or you can just comma-separate 
-// those airports if you don't like the arrows. Whatever method you choose, 
-// you should produce the proper route of the trip. It's guaranteed that the itineraries 
-// have exactly one start airport, and exactly one end airport, and that there aren't any loops. 
-// Basically, the itinerary will form a single linked list.
-
-// return a single string of the mapped itineraries
-
-function mappingItineraries(arr) {
-
-    var start =  []
-    var end = []
-
-    // ['ICN', 'LAX'], ['LAX', 'SFO'], ['SJC', 'ICN'], ['NYU', 'SJC'], ['AMS', 'NYU'] 
-    // organize start and endpoints
-    for(var i = 0 ; i < arr.length ; i ++ ) {
-        start.push(arr[i][0]);
-        // [ICN, LAX, SJC, NYU, AMS]
-        end.push(arr[i][1]);
-        // [LAX, SFO, ICN, SJC, NYU]
-    }
-
-    // find the start point by comparing and finding the outlier
-    var j = 0;
-    var k = 0;
-    while(k < end.length) {
-        if(start[j] == end[k]) {
-            j++;
-            k = 0;
-        } else {
-            k ++;
-        }
-    }
-
-    // start[j] == "AMS"
-    // construct final order from start/endpoints
-    // order = [AMS, NYU, SJC, ICN, LAX, SFO]
-    // start =  []
-    // end =    []
-    var order = [];
-    order.push(start[j], end[j]);
-    start.splice(j, 1);
-    end.splice(j, 1);
-
-    var m = 0;
-    while(end.length > 0) {
-        if(start[m] == order[order.length-1]) {
-            start.splice(m, 1); // remove from 'start' array
-            order.push(end[m]);
-            end.splice(m, 1); // remove from 'end' array
-            m = 0;
-        } else {
-            m ++;
-        }
-    }
-    return order.join(" -> ")
-}
-
-console.log(mappingItineraries( [  ['ICN', 'LAX'], ['LAX', 'SFO'], ['SJC', 'ICN'], ['NYU', 'SJC'], ['AMS', 'NYU'] ]));
-// AMS -> NYU -> SJC -> ICN -> LAX -> SFO
-
-
-// ############################################################################################
-
-
 // There are 3 edits that can be done on a string: add a acharacter, remove a character, or replace a character. 
 // Given 2 strings, write a function that checks if they are one edit or fewer away from eachother (see samples below)
 function oneAway(str1, str2) {
@@ -118,10 +47,21 @@ function oneAway(str1, str2) {
     }
 }
 
-// console.log(oneAway("hello", "eello")) // true
-// console.log(oneAway("hello", "eelloo")) // false 
-// console.log(oneAway("ello", "hello")) // true
-// console.log(oneAway("helllo", "hello")) // true
-// console.log(oneAway("hello", "helo")) // true
-// console.log(oneAway("hello", "hell")) // true
-// console.log(oneAway("hjllo", "helo")) // false
+console.log(oneAway("hello", "eello")) // true
+console.log(oneAway("hello", "eelloo")) // false 
+console.log(oneAway("ello", "hello")) // true
+console.log(oneAway("helllo", "hello")) // true
+console.log(oneAway("hello", "helo")) // true
+console.log(oneAway("hello", "hell")) // true
+console.log(oneAway("hjllo", "helo")) // false
+
+
+// ==========================================
+// Vampires and the sleeping man
+// ==========================================
+
+// Consider a village with vampires and a sleeping man (who never wakes up, no matter what).
+// A vampire can eat the sleeping man, but after eating him, the vampire falls asleep.
+// Similarly, any vampire can eat any other sleeping vampire, and this process repeats.
+// Assume that the vampires are very smart and would ALWAYS choose to stay alive than to eat the man and risk their lives.
+// Initially, there are 65 vampires and 1 sleeping man. What would happen in the village?
